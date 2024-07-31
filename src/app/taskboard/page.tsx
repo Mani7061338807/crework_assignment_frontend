689060b6a6a318510492a60169437475c9952efa
Manishankar Kumar <manishankarkumar789@gmail.com>
2024-07-31 10:18:07 +0530

    ui

--- src/app/taskboard/page.tsx
+++ src/app/taskboard/page.tsx
@@ -1,27 +1,22 @@
-"use client";
+"use client"
+import TaskBoardContent from "@/components/TaskBoardContent";
 import { checkAuthStatus } from "@/helpers/api-communicator";
 import { userAction } from "@/redux/action/user";
 import { useAppDispatch } from "@/redux/hook";
 import { useEffect } from "react";
-import { useRouter } from "next/navigation";
 
 export default function Home() {
   const dispatch = useAppDispatch();
-  const router = useRouter();
   const getUser = async () => {
     const res = await checkAuthStatus();
     dispatch(userAction(res.name, res.email));
   };
-  const handleLogin = () => {
-    router.push('/login')
-  }
   useEffect(() => {
     getUser();
   }, []);
-
   return (
     <div>
-      hello world <div className="cursor-pointer" onClick={handleLogin}>login</div>
+      <TaskBoardContent />
     </div>
   );
 }
